7164144b83b484ba3fef2f0e2e11301d4da1fcc6
Cosmic-G-G <140998316+Cosmic-G-G@users.noreply.github.com>
2025-10-26 19:28:39 -0600

    organizer bottom bar

--- LotteryApp/app/src/main/java/com/example/lotteryapp/MainActivity.java
+++ LotteryApp/app/src/main/java/com/example/lotteryapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lotteryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Spinner s = findViewById(R.id.main_role_dropdown);
+
+        String[] roles = {"Entrant", "Organizer", "Admin"};
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, roles);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        s.setAdapter(adapter);
+
+        Button b = findViewById(R.id.main_signup_button);
+
+        b.setOnClickListener(v -> {
+            switch (s.getSelectedItem().toString()) {
+                case "Entrant":
+                    startActivity(new Intent(this, EntrantActivity.class));
+                    finish();
+                    break;
+                case "Organizer":
+                    startActivity(new Intent(this, OrganizerActivity.class));
+                    finish();
+                    break;
+                case "Admin":
+                    startActivity(new Intent(this, AdminActivity.class));
+                    finish();
+                    break;
+            }
         });
     }
 }
